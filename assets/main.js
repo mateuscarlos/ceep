0d8ddaf35368f2abb8b86a52bd54ece605b554ec
MateusCarlos <mateuscarlos.ti@gmail.com>
2022-01-23 17:30:42 -0300

    Delete main.js

--- assets/main.js
+++
@@ -1,8 +0,0 @@
-import { handleNovoItem } from "../componentes/criaTarefa.js";
-import { carregaTarefa } from "../componentes/carregaTarefa.js";
-
-const novaTarefa = document.querySelector("[data-form-button]");
-
-novaTarefa.addEventListener("click", handleNovoItem);
-
-carregaTarefa();
